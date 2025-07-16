c2c0569f0c23783802201ffd71686ee951344c7d
BijuThomas-NOAA <biju.thomas@noaa.gov>
2025-07-16 19:03:19 +0000

    Fixing a comment in utils_common.lua

--- modulefiles/utils_common.lua
+++ modulefiles/utils_common.lua
@@ -1,6 +1,6 @@
 whatis("Description: UFS_UTILS build environment common libraries")
 
-help([[Load UFS Model common libraries]])
+help([[Load UFS_UTILS common libraries]])
 
 local ufs_modules = {
   {["bacio"]           = "2.4.1"},
